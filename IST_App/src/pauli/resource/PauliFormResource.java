f9f3fc4c8447387c072063136c32fa9955952643
Edward Vito <129828532+Trustacean@users.noreply.github.com>
2024-12-03 17:57:47 +0700

    feat: add static fields and resetValues method to PauliFormResource for managing performance metrics

--- IST_App/src/pauli/resource/PauliFormResource.java
+++ IST_App/src/pauli/resource/PauliFormResource.java
@@ -1,5 +1,25 @@
 package pauli.resource;
 
 public class PauliFormResource {
-    
+    public static int kemampuanKerja;
+    public static int kemampuanAdaptasi;
+    public static int ketekunan;
+    public static int konsentrasi;
+    public static int manajemenEmosi;
+    public static int motivasiBerprestasi;
+
+    static {
+        resetValues();
+    }
+
+    public static void resetValues() {
+        kemampuanKerja = 0;
+        kemampuanAdaptasi = 0;
+        ketekunan = 0;
+        konsentrasi = 0;
+        manajemenEmosi = 0;
+        motivasiBerprestasi = 0;
+    }
+
+        
 }
