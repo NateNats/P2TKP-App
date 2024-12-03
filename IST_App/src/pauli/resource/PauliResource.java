8f873f5f54851814322c755da20f2f9e75db9cb7
Edward Vito <129828532+Trustacean@users.noreply.github.com>
2024-12-03 21:04:01 +0700

    feat: implement report generation in PauliResource for performance metrics

--- IST_App/src/pauli/resource/PauliResource.java
+++ IST_App/src/pauli/resource/PauliResource.java
@@ -1,5 +1,15 @@
 package pauli.resource;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import net.sf.jasperreports.engine.JREmptyDataSource;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.view.JasperViewer;
+
 public class PauliResource {
     public static int tingkatPercayaDiri;
     public static int kemampuanKerja;
@@ -9,11 +19,14 @@ public class PauliResource {
     public static int manajemenEmosi;
     public static int motivasiBerprestasi;
 
+    private final static String REPORT_PATH = "./src/pauli/resource/report/PauliReport_v1.jasper";
+
     static {
         resetValues();
     }
 
     public static void resetValues() {
+        tingkatPercayaDiri = 0;
         kemampuanKerja = 0;
         kemampuanAdaptasi = 0;
         ketekunan = 0;
@@ -22,5 +35,29 @@ public class PauliResource {
         motivasiBerprestasi = 0;
     }
 
-        
+    public static void produceReport() {
+        File file = new File(REPORT_PATH);
+
+        if (file.exists()) {
+            Map<String, Object> parameters = new HashMap<>();
+            parameters.put("tingkatPercayaDiri", PauliScoreInterpreter.getInterpretation("tingkat percaya diri", tingkatPercayaDiri));
+            parameters.put("kemampuanKerja", PauliScoreInterpreter.getInterpretation("kemampuan kerja", kemampuanKerja));
+            parameters.put("kemampuanAdaptasi", PauliScoreInterpreter.getInterpretation("kemampuan adaptasi", kemampuanAdaptasi));
+            parameters.put("ketekunan", PauliScoreInterpreter.getInterpretation("ketekunan", ketekunan));
+            parameters.put("konsentrasi", PauliScoreInterpreter.getInterpretation("konsentrasi", konsentrasi));
+            parameters.put("manajemenEmosi", PauliScoreInterpreter.getInterpretation("manajemen emosi", manajemenEmosi));
+            parameters.put("motivasiBerprestasi", PauliScoreInterpreter.getInterpretation("motivasi berprestasi", motivasiBerprestasi));
+            parameters.put("ketelitian", PauliScoreInterpreter.getKetelitianInterpretation(ketekunan, konsentrasi));
+
+            try {
+                JasperPrint jasperPrint = JasperFillManager.fillReport(REPORT_PATH, parameters,
+                        new JREmptyDataSource());
+                JasperViewer.viewReport(jasperPrint, false);
+            } catch (JRException e) {
+                System.err.println("Could not produce report.");
+            }
+        } else {
+            System.err.println("Report file not found.");
+        }
+    }
 }
